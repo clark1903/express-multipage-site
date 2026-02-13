19f264fbdc938cec1f9174f3e9849e3fdb3b73e1
Clark Barrientos <mcgbarrientos@tip.edu.ph>
2026-02-13 08:50:54 +0800

    Initial Express site

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+// serve static files (CSS, images, JS) from /public
+app.use(express.static(path.join(__dirname, "public")));
+
+// routes to HTML files in /views
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "about.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "contact.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
